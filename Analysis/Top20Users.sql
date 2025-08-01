1963d7cb782b08f97f7156f0b4c1c16015a899fb
Siddhant Mene <162431406+siddhantmene1@users.noreply.github.com>
2025-08-01 08:37:27 -0700

    Create Top20Users.sql

---
+++ Analysis/Top20Users.sql
@@ -0,0 +1,8 @@
+SELECT
+    user_id,
+    COUNT(*) AS number_of_ratings,
+    AVG(rating) AS average_rating_given
+FROM MOVIELENS.DEV.FCT_RATINGS
+GROUP BY user_id
+ORDER BY number_of_ratings DESC
+LIMIT 20;
